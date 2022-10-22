ba44fdfcd73aae06245f120bc0f30bc59edff01e
haithemelhadj <haithemelhadj42@gmail.com>
2022-10-23 00:25:12 +0100

    fixed bird

--- Assets/BirdMovement.cs
+++ Assets/BirdMovement.cs
@@ -6,10 +6,12 @@ public class BirdMovement : MonoBehaviour
 {
     private bool Down = false;
     private int[] list = { -1, 1 };
+    private int random;
+    private float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        random = list[Random.Range(0, list.Length)];
     }
 
     // Update is called once per frame
@@ -18,7 +20,21 @@ public class BirdMovement : MonoBehaviour
         if(!Down)
         {
             // continusly move left and right between screen borders
-            transform.position = new Vector3(list[Random.Range(0, list.Length)] * Mathf.PingPong(Time.time * 2, 4) - 2, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(list[Random.Range(0, list.Length)] * Mathf.PingPong(Time.time * 2, 4) - 2, transform.position.y, transform.position.z);
+            //translate betwwen 2 x points
+            if(transform.position.x >= Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x - 0.4f)
+            {
+                random *= -1;
+
+            }
+            else if (transform.position.x <= -Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x+0.4f)
+            {
+                random *= -1;
+            }
+
+            transform.Translate(random * speed * Time.deltaTime, 0f, 0f);
+
+
         }
         else
         {
